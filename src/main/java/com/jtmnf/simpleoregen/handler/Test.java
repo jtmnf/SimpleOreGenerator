298a2f7226329248a32f5a4a1f61a0d8ab33e162
jtmnf <jtmnf@hotmail.com>
2016-05-18 01:32:14 +0100

    Updated to latest Forge Version;

--- src/main/java/com/jtmnf/simpleoregen/handler/Test.java
+++
@@ -1,7 +0,0 @@
-package com.jtmnf.simpleoregen.handler;
-
-import net.minecraftforge.fml.common.IWorldGenerator;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
-public class Test extends GameRegistry {
-}
