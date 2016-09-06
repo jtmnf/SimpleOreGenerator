d21f48d8e7b40b9bb841431454385113462bc33a
DESKTOP-MN9VJ3I\jtmnf <jtmnf@hotmail.com>
2016-09-06 09:06:34 +0100

    Updated repository; Removed a lot of trash;

--- src/main/java/com/jtmnf/simpleoregen/SimpleOreGen.java
+++ src/main/java/com/jtmnf/simpleoregen/SimpleOreGen.java
@@ -28,7 +28,7 @@ public class SimpleOreGen {
     /* ===== MOD INFO ===== */
     public static final String MOD_NAME = "SimpleOreGen";
     public static final String MOD_ID = "simpleoregen";
-    public static final String MOD_VERSION = "1.10.2-0.4.1";
+    public static final String MOD_VERSION = "1.10.2-0.4.2";
 
     /* ========================== */
     /*  DO NOT TOUCH THIS PLEASE  */
